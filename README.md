04fafe1be34e13ffff0347c0607c60be47eb93bf
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-11-26 16:07:55 -0800

    Setup project structure and postgresql database & test database connection and query

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # inventory-app
+
+In this project I will be creating and Inventory management app for an imaginary store. This app will have categorues and items, so when the user goes to the home-page they can view items by category.
+
+I will include all of the CRUD methods for both items and categories as well.
+
+For this project from The Odin Project, I will be using NodeJS and Express to build this inventory app that incorporates key skills like database management, API design, and CRUD functionality.
---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const pool = require("../db/pool");
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Parse JSON requests
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Inventory App is running!");
+});
+
+// Test database query
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM categories");
+    res.json(result.rows);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Database connection failed");
+  }
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
