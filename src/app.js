6e816a07cf566a13e7cfabfb381176fd46d8cbaa
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-12-07 12:26:10 -0800

    Dynamic port

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use("/categories", categoriesRoutes);
 app.use("/items", itemRoutes);
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
